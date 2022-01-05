4860b39201bbd676da24285195045ba2357a8438
sashanclrp <sashanicolaiclrp@outlook.es>
2022-01-05 16:55:42 -0500

    Juego Terminado con Switch

--- paRoSciGame.js
+++ paRoSciGame.js
@@ -6,31 +6,58 @@ var juego = [
     "Tijera"
 ]
 
-function paRoSci (jugador) {
-    var i = Math.floor(Math.random()*(2-0+1))
+function paRoSciIf (jugador) {
+    var i = Math.floor(Math.random()*(2-0+1));
     if (juego[i]==="Piedra" && jugador === "Papel"){
-        console.log("Jugador: " + jugador)
-        console.log("Máquina: " + juego[i])
-        console.log ("Haz ganado")
+        console.log("Jugador: " + jugador);
+        console.log("Máquina: " + juego[i]);
+        console.log ("Haz ganado");
     } 
     else if (juego[i]==="Tijera" && jugador === "Piedra") {
-        console.log("Jugador: " + jugador)
-        console.log("Máquina: " + juego[i])
-        console.log ("Haz ganado")
+        console.log("Jugador: " + jugador);
+        console.log("Máquina: " + juego[i]);
+        console.log ("Haz ganado");
     }
     else if (juego[i]==="Papel" && jugador === "Tijera") {
-        console.log("Jugador: " + jugador)
-        console.log("Máquina: " + juego[i])
-        console.log ("Haz ganado")
+        console.log("Jugador: " + jugador);
+        console.log("Máquina: " + juego[i]);
+        console.log ("Haz ganado");
     }
     else if (juego[i]=== jugador) {
-        console.log("Jugador: " + jugador)
-        console.log("Máquina: " + juego[i])
-        console.log ("Haz Empatado")
+        console.log("Jugador: " + jugador);
+        console.log("Máquina: " + juego[i]);
+        console.log ("Haz Empatado");
     }
     else{
-        console.log("Jugador: " + jugador)
-        console.log("Máquina: " + juego[i])
-        console.log ("Haz perdido")
+        console.log("Jugador: " + jugador);
+        console.log("Máquina: " + juego[i]);
+        console.log ("Haz perdido");
     } 
+}
+
+//Generar el mismo juego pero usando Switch
+
+function paRoSciSwitch (jugador) {
+    var i = Math.floor(Math.random()*(2-0+1))
+    switch (true){
+        case (jugador===juego[i]):
+            console.log("Jugador: " + jugador);
+            console.log("Máquina: " + juego[i]);
+            console.log ("Haz Empatado");
+        break;
+
+        case (juego[i]==="Piedra" && jugador === "Papel"):
+        case (juego[i]==="Tijera" && jugador === "Piedra"):
+        case (juego[i]==="Papel" && jugador === "Tijera"):
+            console.log("Jugador: " + jugador);
+            console.log("Máquina: " + juego[i]);
+            console.log ("Haz ganado");
+        break;
+
+        default:
+            console.log("Jugador: " + jugador);
+            console.log("Máquina: " + juego[i]);
+            console.log ("Haz perdido");
+
+    }
 }
