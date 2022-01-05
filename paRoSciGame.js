211b0215849fd69df0182fb8fbfa2078cdd34f24
sashanclrp <sashanicolaiclrp@outlook.es>
2022-01-05 14:33:43 -0500

    Juego Terminado

--- paRoSciGame.js
+++ paRoSciGame.js
@@ -6,6 +6,31 @@ var juego = [
     "Tijera"
 ]
 
-if (juego[0]==="Piedra"){
-    console.log ("Haz ganado")
+function paRoSci (jugador) {
+    var i = Math.floor(Math.random()*(2-0+1))
+    if (juego[i]==="Piedra" && jugador === "Papel"){
+        console.log("Jugador: " + jugador)
+        console.log("Máquina: " + juego[i])
+        console.log ("Haz ganado")
+    } 
+    else if (juego[i]==="Tijera" && jugador === "Piedra") {
+        console.log("Jugador: " + jugador)
+        console.log("Máquina: " + juego[i])
+        console.log ("Haz ganado")
+    }
+    else if (juego[i]==="Papel" && jugador === "Tijera") {
+        console.log("Jugador: " + jugador)
+        console.log("Máquina: " + juego[i])
+        console.log ("Haz ganado")
+    }
+    else if (juego[i]=== jugador) {
+        console.log("Jugador: " + jugador)
+        console.log("Máquina: " + juego[i])
+        console.log ("Haz Empatado")
+    }
+    else{
+        console.log("Jugador: " + jugador)
+        console.log("Máquina: " + juego[i])
+        console.log ("Haz perdido")
+    } 
 }
