e5e82716bd297d9afd6a877bf558bf2fde00737c
sashanclrp <sashanicolaiclrp@outlook.es>
2022-01-05 23:05:15 -0500

    Mi Primera colección teórica de NFTs

---
+++ CarNFTGen.js
@@ -0,0 +1,49 @@
+// Crear una función que te genere todas las posibles combinaciones
+// De carros según 3 parámetros
+
+var brand = [
+    "Tesla",
+    "Ferrari",
+    "Lamborgini",
+    "Aston Martin"
+]
+
+var color = [
+    "Rojo",
+    "Azul",
+    "Negro",
+    "Blanco"
+]
+
+var model = [
+    2022,
+    2021,
+    2020,
+    2019
+]
+
+function luxCar (br, co, mo) {
+    this.brand = br;
+    this.color = co;
+    this.model = mo;
+}
+
+var i = 0
+var carId = 0
+
+while (i < brand.length){
+    var x = 0
+    while (x < color.length){
+        var y = 0
+        while (y < model.length){
+            window["luxuryCarId_"+carId] = new luxCar(brand[i],color[x],model[y])
+            console.log(window["luxuryCarId_"+carId])
+            y++
+            carId++
+        }
+
+        x++
+    }
+
+    i++
+}
